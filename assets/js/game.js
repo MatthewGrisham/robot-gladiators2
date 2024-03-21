1b329a06806bc8dc5e74cea518e839290e31f593
Matt Grisham <magrishams@gmail.com>
2024-03-21 13:42:06 -0500

    started outline for shop funciton

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,93 +7,85 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function(enemyName) {
-  while (playerHealth > 0 && enemyHealth > 0) {
-    // ask player if they'd like to fight or run
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.'); 
-    // if player picks "skip" confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
-      // confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-         
-      // if yes (true), leave fight
-      if (confirmSkip) {
-        window.alert(playerName + ' has decided to skip this fight. Goodbye!');
-        // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
-        console.log("playerMoney", playerMoney)
-        break;
-   }
-  }  
-  
-  // remove enemy's health by subtracting the amount set in the playerAttack variable
+var fight = function() {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
+
+  // ask player if they'd like to fight or run
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  // if player choses to fight, fight
+  if (promptFight === "fight" || promptFight === "FIGHT") {
+    // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
-       
+
     // check enemy's health
     if (enemyHealth <= 0) {
       window.alert(enemyName + " has died!");
+    } else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
 
-      // award player money for winning
-      playerMoney = playerMoney + 20;
-      // leave while() loop since enemy is dead
-      break;
-      } else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
-      }
-        
-        // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
-        console.log(
-          enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-          );
-          
-          // check player's health
-          if (playerHealth <= 0) {
-            window.alert(playerName + " has died!");
-            // leave while() loop if player is dead
-            break;
-          } else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
-          }
-  } // end of while loop
+    // remove players's health by subtracting the amount set in the enemyAttack variable
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    // check player's health
+    if (playerHealth <= 0) {
+      window.alert(playerName + " has died!");
+    } else {
+      window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+    // if player choses to skip
+  } else if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
+      fight();
+    }
+    // if player did not chose 1 or 2 in prompt
+  } else {
+    window.alert("You need to pick a valid option. Try again!");
+  }
 }; // end of fight function
 
+// shop function
 
-// Game States
+var shop = function {
+  var promptShop = window.prompt("Would you like to REFILL your health, UPGRADE your attack or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice")
+}; // end of shop function
 
-// "WIN" - Player robot has defeated all enemy-robots
-//       - Fight all enemy-robots
-//       - Defeat Each enemy-robot
-// "LOSE"- Player robot's health is zero or less
+// After beating of skipping a robot ask if they want to visit the shop
+
+// if yes then open the shop offer them UPGRADE their attack, REFILL health, or LEAVE 
+
+// uppon leaving update player attack or health if they UPGRADE or REFILL
+
+// start next roung.
+
+// Once all robots are defeated present player stats and ask if they want to play again
+
+// if yes call the fight function again.
+
 // run fight function to start game
+fight();
 
-for(var i = 0; i < enemyNames.length; i++) {
-  if (playerHealth > 0) {
-    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert("Welcome to Robot gladiators! Round " + ( i + 1 ) );
-
-    // pick new enemy to fight based on teh index of the enemyNames array
-    var pickedenemyName = enemyNames[i]
-
-    // reset enemyHealth before starting new fight
-    enemyHealth = 50;
-
-    // use debugger to puase script form running and check what's goign on at that moment in the code
-    // debugger;
-
-    // pass the pickedenemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(pickedenemyName);
-
-  } else {
-    window.alert("You have lost your robot in battle! Game Over!");
-    break;
-  }
-}
+shop();
